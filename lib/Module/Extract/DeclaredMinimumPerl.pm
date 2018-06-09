596907aa4c8f4afecb670e6ecc329db0a432861b
brian d foy <brian.d.foy@gmail.com>
2018-06-09 08:55:59 -0400

    Bump copyright year

--- lib/Module/Extract/DeclaredMinimumPerl.pm
+++ lib/Module/Extract/DeclaredMinimumPerl.pm
@@ -171,7 +171,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2011-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
