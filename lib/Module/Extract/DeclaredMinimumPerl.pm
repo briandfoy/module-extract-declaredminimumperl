0ae5c9718a47716ad2eecd0d02cdcd938f59f8ff
brian d foy <briandfoy@pobox.com>
2026-01-07 10:12:53 -0500

    Updated copyright year

--- lib/Module/Extract/DeclaredMinimumPerl.pm
+++ lib/Module/Extract/DeclaredMinimumPerl.pm
@@ -171,7 +171,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2011-2025, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2011-2026, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
